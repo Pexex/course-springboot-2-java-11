906ad4f8b3c00418b511461eee8782dfa1c815a4
Roberto Peixinho <rhpeixinho@gmail.com>
2022-04-13 15:32:18 -0300

    Product

---
+++ src/main/java/com/rhvp/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.rhvp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rhvp.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
